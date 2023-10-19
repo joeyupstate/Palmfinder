c0ef21eb548a2c9087b064c0573f945f57ae5813
joeyupstate <josephabele2@gmail.com>
2023-10-19 14:54:22 -0400

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import searchIcon from "./assets/search-icon.png";
 function App() {
   const [zipData, setZipData] = useState([]);
   const [zone, setZone] = useState();
-  const [palms, setPalms] = useState();
+  const [palmTrees, setPalmTrees] = useState();
 
   async function checkZip() {
     const url = `https://plant-hardiness-zone.p.rapidapi.com/zipcodes/${zipData}`;
@@ -23,32 +23,15 @@ function App() {
       const result = await response.json();
       console.log(result.hardiness_zone);
       setZone(result.hardiness_zone);
-      const palmList = palmData.find((palm) => palm.id === zone);
+      const palmList = palmData.find((palm) => palm.id === zone); //this searches the palmdata for the matching zone
 
-      for (let i = 0; i < palmList.palms.length; i++) {
-        console.log(palmList.palms[i]);
-        setPalms(palmList.palms[i]);
-      }
+      setPalmTrees(palmList.palms);
+      // const listItems = palmTrees.map((pt) => <li>{pt}</li>);
     } catch (error) {
       console.error(error);
     }
   }
-  // const object = palmData.find((obj) => obj.id === "7a");
-  // const object2 = object.palms;
-
-  // const test = () => {
-  //   for (let i = 0; i < result2.length; i++) {
-  //     console.log(result2[i]);
-  //   }
-  // };
-  // const returnPalms = () => {
-  //   // palmData.map((palm) => {
-  //   //   if (zone !== [palm.id]) {
-  //   //     palm.id;
-  //   //   }
-  //   // });
-  //   palmData.filter((palm) => [palm.id] === zone);
-  // };
+  const listItems = palmTrees.map((pt) => <li>{pt}</li>);
 
   return (
     <div className="app">
@@ -57,8 +40,7 @@ function App() {
         <img src={searchIcon} onClick={checkZip} className="search-icon" />
       </div>
       <div>{zone}</div>
-      {/* <div>{palms}</div> */}
-      <ul>map</ul>
+      <ul className="palm-list">{listItems}</ul>
     </div>
   );
 }
