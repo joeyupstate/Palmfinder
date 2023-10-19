afcb14864353b7386587ea9fd132f805021c96b5
joeyupstate <josephabele2@gmail.com>
2023-10-19 09:57:41 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import React, { useState } from "react";
 import Search from "./components/Search";
+import { palmData } from "./Data";
+import searchIcon from "./assets/search-icon.png";
 
 function App() {
   const [zipData, setZipData] = useState([]);
+  const [zone, setZone] = useState();
+  const [palms, setPalms] = useState();
 
-  async function hey() {
+  async function checkZip() {
     const url = `https://plant-hardiness-zone.p.rapidapi.com/zipcodes/${zipData}`;
     const options = {
       method: "GET",
@@ -16,21 +20,45 @@ function App() {
     };
     try {
       const response = await fetch(url, options);
-      const result = await response.text();
-      console.log(result);
+      const result = await response.json();
+      console.log(result.hardiness_zone);
+      setZone(result.hardiness_zone);
+      const palmList = palmData.find((palm) => palm.id === zone);
+
+      for (let i = 0; i < palmList.palms.length; i++) {
+        console.log(palmList.palms[i]);
+        setPalms(palmList.palms[i]);
+      }
     } catch (error) {
       console.error(error);
     }
   }
+  // const object = palmData.find((obj) => obj.id === "7a");
+  // const object2 = object.palms;
 
-  const test = () => {
-    console.log(zipData);
-  };
+  // const test = () => {
+  //   for (let i = 0; i < result2.length; i++) {
+  //     console.log(result2[i]);
+  //   }
+  // };
+  // const returnPalms = () => {
+  //   // palmData.map((palm) => {
+  //   //   if (zone !== [palm.id]) {
+  //   //     palm.id;
+  //   //   }
+  //   // });
+  //   palmData.filter((palm) => [palm.id] === zone);
+  // };
 
   return (
-    <div>
-      <Search setZipData={setZipData} />
-      <button onClick={hey}>click</button>
+    <div className="app">
+      <div className="search-bar">
+        <Search setZipData={setZipData} />
+        <img src={searchIcon} onClick={checkZip} className="search-icon" />
+      </div>
+      <div>{zone}</div>
+      {/* <div>{palms}</div> */}
+      <ul>map</ul>
     </div>
   );
 }
--- src/Data.js
+++ src/Data.js
@@ -1,7 +1,59 @@
 // Trachycarpus fortunei, Windmill Palm https://en.wikipedia.org/wiki/Trachycarpus_fortunei
 
-// const palmData =[
-//     {name:"Trachycarpus fortunei",
-// link:'https://en.wikipedia.org/wiki/Trachycarpus_fortunei'},
+export const palmData = [
+  {
+    id: "7a",
+    palms: [
+      "Trachycarpus fortunei",
+      "sabal-minor",
+      "sabal-palmetto",
+      "pindo-palm",
+    ],
+  },
+  {
+    id: "7b",
+    palms: [
+      "Trachycarpus fortunei",
+      "sabal-minor",
+      "sabal-palmetto",
+      "pindo-palm",
+      "sago-palm",
+      "cabbage-palmetto",
+    ],
+  },
+  {
+    id: "8a",
+    palms: [
+      "Trachycarpus fortunei",
+      "sabal-minor",
+      "sabal-palmetto",
+      "pindo-palm",
+      "sago-palm",
+      "cabbage-palmetto",
+      "Blue Hesper Palm",
+      "Mediterranian Fan Palm",
+      "Chilean Wine Palm",
+      "Chinese Fan Palm",
 
-// ]
+      "Saw Palmetto",
+    ],
+  },
+  {
+    id: "8b",
+    palms: [
+      "Trachycarpus fortunei",
+      "sabal-minor",
+      "sabal-palmetto",
+      "pindo-palm",
+      "sago-palm",
+      "cabbage-palmetto",
+      "Blue Hesper Palm",
+      "Mediterranian Fan Palm",
+      "Chilean Wine Palm",
+      "Chinese Fan Palm",
+      "Canary Island Date Palm",
+      "Saw Palmetto",
+      "California Fan Palm",
+    ],
+  },
+];
